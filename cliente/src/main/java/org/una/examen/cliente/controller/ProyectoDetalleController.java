b02ee1c36485de67a5c32c088d3e64d16cb6a187
Pablo-VE <53917989+Pablo-VE@users.noreply.github.com>
2020-10-28 22:45:37 -0600

    Update Proyectos Pablo

--- cliente/src/main/java/org/una/examen/cliente/controller/ProyectoDetalleController.java
+++ cliente/src/main/java/org/una/examen/cliente/controller/ProyectoDetalleController.java
@@ -7,19 +7,35 @@ package org.una.examen.cliente.controller;
 
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import static javafx.scene.paint.Color.RED;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import org.una.examen.cliente.App;
 import org.una.examen.cliente.dto.ProyectoDTO;
 import org.una.examen.cliente.dto.TareaDTO;
 import org.una.examen.cliente.service.ProyectoService;
+import org.una.examen.cliente.service.TareaService;
 import org.una.examen.cliente.util.AppContext;
 import org.una.examen.cliente.util.Mensaje;
 import org.una.examen.cliente.util.Respuesta;
@@ -58,15 +74,22 @@ public class ProyectoDetalleController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+    
+    boolean creado=false;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         modalidad=(String) AppContext.getInstance().get("ModalidadProyecto");
+        cargarTabla();
         if(modalidad.equals("Agregar")){
             lbFechaRegistro.setVisible(false);
             btnAgregarTarea.setDisable(true);
             btnAgregarTarea.setVisible(false);
             proyecto = new ProyectoDTO();
             lbTitulo.setText("CREACIÓN DE PROYECTO");
+        }else{
+            creado=true;
         }
         // TODO
     }    
@@ -95,9 +118,16 @@ public class ProyectoDetalleController implements Initializable {
                     btnGuardar.setVisible(false);
                     btnAgregarTarea.setVisible(true);
                     btnAgregarTarea.setDisable(false);
+                    creado=true;
+                    txtNombre.setDisable(true);
+                    txtDescripcion.setDisable(true);
+                    txtResponsable.setDisable(true);
+                   
                 }else{
                     Mensaje.showAndWait(Alert.AlertType.INFORMATION, "Registro de proyecto", "Ocurrió un error al registrar su proyecto");
                 }
+            }else{
+                
             }
         }
     }
@@ -117,6 +147,96 @@ public class ProyectoDetalleController implements Initializable {
 
     @FXML
     private void actAgregarTarea(ActionEvent event) {
+        if(creado){
+            try{
+                Stage stage = new Stage();
+                AppContext.getInstance().set("ModalidadTarea", "Agregar");
+                AppContext.getInstance().set("Proyecto", proyecto);
+                AppContext.getInstance().set("VistaProyecto", "Creacion");
+                AppContext.getInstance().set("ControllerProyecto", this);
+                Parent root = FXMLLoader.load(App.class.getResource("TareasDetalle" + ".fxml"));
+                stage.setScene(new Scene(root));
+                stage.setTitle("Registro de tarea");
+                stage.initModality(Modality.WINDOW_MODAL);
+                stage.initOwner(
+                    ((Node)event.getSource()).getScene().getWindow() );
+                stage.show();
+            }catch(IOException ex){
+                System.out.println(ex.getMessage());
+                Mensaje.showAndWait(Alert.AlertType.ERROR, "Opps :c", "Se ha producido un error inesperado en la aplicación");
+            };
+        }
+    }
+    
+    public void cargarTabla(){
+        TareaService tareaService = new TareaService();
+        ArrayList<TareaDTO> tareas = new ArrayList<TareaDTO>();
+        Respuesta respuesta = tareaService.getByProyecto(proyecto.getId());
+        if(respuesta.getEstado().equals(true)){
+            tareas = (ArrayList<TareaDTO>) respuesta.getResultado("Tareas");
+            
+        }
+        
+        tvTareas.getColumns().clear();
+        if(!tareas.isEmpty()){
+            ObservableList items = FXCollections.observableArrayList(tareas);   
+            
+            TableColumn <TareaDTO, Long>colId = new TableColumn("ID");
+            colId.setCellValueFactory(new PropertyValueFactory("id"));
+            
+            TableColumn <TareaDTO, String>colNombre = new TableColumn("Nombre");
+            colNombre.setCellValueFactory(new PropertyValueFactory("nombre"));
+            
+            TableColumn <TareaDTO, String>colPrioridad = new TableColumn("Prioridad");
+            colPrioridad.setCellValueFactory(ta -> {
+                String prioridad = String.valueOf(ta.getValue().getImportancia()*ta.getValue().getUrgencia());
+                return new ReadOnlyStringWrapper(prioridad);
+            });
+            
+            TableColumn <TareaDTO, String>colFechaInicio = new TableColumn("Fecha de inicio");
+            colFechaInicio.setCellValueFactory(ta -> {
+                SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+                String fecha = formatter.format(ta.getValue().getFechaInicio());
+                return new ReadOnlyStringWrapper(fecha);
+            });
+            
+            TableColumn <TareaDTO, String>colFechaFinalizacion = new TableColumn("Fecha de finalización");
+            colFechaFinalizacion.setCellValueFactory(ta -> {
+                SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+                String fecha = formatter.format(ta.getValue().getFechaFinalizacion());
+                return new ReadOnlyStringWrapper(fecha);
+            });
+            
+            TableColumn<TareaDTO, String> colAvance = new TableColumn("Porcentaje de avance");
+            colAvance.setCellValueFactory(ta -> {
+                String avance = String.valueOf(ta.getValue().getPorcentajeAvance()+"%");
+                return new ReadOnlyStringWrapper(avance);
+            });
+            
+            
+            tvTareas.getColumns().addAll(colId);
+            tvTareas.getColumns().addAll(colNombre);
+            tvTareas.getColumns().addAll(colPrioridad);
+            tvTareas.getColumns().addAll(colAvance);
+            tvTareas.getColumns().addAll(colFechaInicio);
+            tvTareas.getColumns().addAll(colFechaFinalizacion);
+            tvTareas.setItems(items);
+        }else{
+            TableColumn <TareaDTO, Long>colId = new TableColumn("ID");
+            TableColumn <TareaDTO, String>colNombre = new TableColumn("Nombre");
+            TableColumn <TareaDTO, String>colPrioridad = new TableColumn("Prioridad");
+            TableColumn <TareaDTO, String>colFechaInicio = new TableColumn("Fecha de inicio");
+            TableColumn <TareaDTO, String>colFechaFinalizacion = new TableColumn("Fecha de finalización");
+            TableColumn<TareaDTO, String> colAvance = new TableColumn("Porcentaje de avance");
+            
+            
+            tvTareas.getColumns().add(colId);
+            tvTareas.getColumns().add(colNombre);
+            tvTareas.getColumns().add(colPrioridad);
+            tvTareas.getColumns().add(colAvance);
+            tvTareas.getColumns().add(colFechaInicio);
+            tvTareas.getColumns().add(colFechaFinalizacion);
+        }
     }
     
 }
--- cliente/src/main/java/org/una/examen/cliente/controller/TareasDetalleController.java
+++ cliente/src/main/java/org/una/examen/cliente/controller/TareasDetalleController.java
@@ -11,11 +11,31 @@ import com.jfoenix.controls.JFXSlider;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.stage.Stage;
+import javax.json.bind.annotation.JsonbDateFormat;
+import org.una.examen.cliente.dto.ProyectoDTO;
+import org.una.examen.cliente.dto.TareaDTO;
+import org.una.examen.cliente.service.TareaService;
+import org.una.examen.cliente.util.AppContext;
+import org.una.examen.cliente.util.Mensaje;
+import org.una.examen.cliente.util.Respuesta;
 
 /**
  * FXML Controller class
@@ -35,18 +55,208 @@ public class TareasDetalleController implements Initializable {
     @FXML
     private DatePicker dpFechaFinalizacion;
     @FXML
-    private JFXComboBox<?> cbNivelPrioridad;
+    private JFXSlider nivelAvance;
     @FXML
-    private JFXSlider nivelImportancia;
+    private JFXComboBox<Integer> cbNivelImportancia;
     @FXML
-    private JFXComboBox<?> cbNivelImportancia;
+    private Label lbTituloModificacion;
+    @FXML
+    private Label lbFechaRegistro;
+    @FXML
+    private Label lbFechaModificacion;
+    @FXML
+    private Button btnGuardar;
 
     /**
      * Initializes the controller class.
      */
+    
+    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+    
+    String modalidad="";
+    private ProyectoDTO proyecto = new ProyectoDTO();
+    private TareaDTO tarea = new TareaDTO();
+    private TareaService tareaService = new TareaService();
+    
+    @FXML
+    private Label lbTituloRegistro;
+    @FXML
+    private JFXComboBox<Integer> cbNivelUrgencia;
+    @FXML
+    private Label lbPrioridad;
+    
+    
+    private String modalidadVista = "";
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        initCBX();
+        modalidadVista = (String) AppContext.getInstance().get("VistaProyecto");
+        modalidad = (String) AppContext.getInstance().get("ModalidadTarea");
+        proyecto = (ProyectoDTO) AppContext.getInstance().get("Proyecto");
+        if(modalidad.equals("Agregar")){
+            nivelAvance.setValue(0);
+            lbFechaModificacion.setVisible(false);
+            lbFechaRegistro.setVisible(false);
+            lbTituloModificacion.setVisible(false);
+            lbTituloRegistro.setVisible(false);  
+            cbNivelImportancia.setValue(1);
+            cbNivelUrgencia.setValue(1);
+            establecerPrioridad();
+            lbTitulo.setText("Registro Tarea - Proyecto "+proyecto.getNombre());
+        }else{
+            tarea = (TareaDTO) AppContext.getInstance().get("TareaEnCuestion");
+            lbTitulo.setText("Tarea "+tarea.getNombre()+" - Proyecto "+proyecto.getNombre());
+            txtNombre.setText(tarea.getNombre());
+            if(tarea.getDescipcion()!=null){
+                txtDescripcion.setText(tarea.getDescipcion());
+            }
+            ZoneId defaultZoneId = ZoneId.systemDefault();
+            Instant instant = tarea.getFechaInicio().toInstant();
+            dpFechaInicio.setValue(instant.atZone(defaultZoneId).toLocalDate());
+            instant = tarea.getFechaFinalizacion().toInstant();
+            dpFechaFinalizacion.setValue(instant.atZone(defaultZoneId).toLocalDate());
+            
+            lbFechaRegistro.setText(formatter.format(tarea.getFechaRegistro()));
+            lbFechaModificacion.setText(formatter.format(tarea.getFechaModificacion()));
+            
+            cbNivelImportancia.setValue(tarea.getImportancia());
+            cbNivelUrgencia.setValue(tarea.getUrgencia());
+            establecerPrioridad();
+            nivelAvance.setValue(Double.valueOf(tarea.getPorcentajeAvance()));
+            
+        }
     }    
+    
+    public void establecerPrioridad(){
+        int urgencia = cbNivelImportancia.getValue()*cbNivelUrgencia.getValue();
+        lbPrioridad.setText(String.valueOf(urgencia));
+    }
+    
+    public void initCBX(){
+        ArrayList<Integer> niveles = new ArrayList<Integer>();
+        niveles.add(1);
+        niveles.add(2);
+        niveles.add(3);
+        ObservableList items = FXCollections.observableArrayList(niveles);   
+        cbNivelImportancia.setItems(items); 
+        cbNivelUrgencia.setItems(items); 
+    }
+
+    @FXML
+    private void actGuardar(ActionEvent event) {
+        if(validar()){
+            tarea.setNombre(txtNombre.getText());
+            if(txtDescripcion.getText().isBlank()){
+                if(modalidad.equals("Modificar")){
+                    tarea.setDescipcion("");
+                }
+            }else{
+                tarea.setDescipcion(txtDescripcion.getText());
+            }
+            tarea.setPorcentajeAvance((float)((int)nivelAvance.getValue()));
+            tarea.setImportancia(cbNivelImportancia.getValue());
+            tarea.setUrgencia(cbNivelUrgencia.getValue());
+            tarea.setFechaInicio(fechaInicioGuardar);
+            tarea.setFechaFinalizacion(fechaFinalizacionGuardar);
+            tarea.setProyecto(proyecto);
+            
+            if(modalidad.equals("Modificar")){
+                Respuesta res= tareaService.modificar(tarea.getId(), tarea);
+                if(res.getEstado()){
+                    tarea = (TareaDTO) res.getResultado("Tarea");
+                    Mensaje.showAndWait(Alert.AlertType.INFORMATION, "Modificación de tarea", "La tarea fue modificada con éxito");
+                    Stage stage = (Stage) btnGuardar.getScene().getWindow();
+                    stage.close();
+                }else{
+                    Mensaje.showAndWait(Alert.AlertType.ERROR, "Modificación de tarea", "Hubo un error al modificar la tarea");
+                }
+            }else{
+                if(modalidad.equals("Agregar")){
+                    Respuesta res = tareaService.crear(tarea);
+                    if(res.getEstado()){
+                        tarea = (TareaDTO) res.getResultado("Tarea");
+                        if(modalidadVista.equals("Creacion")){
+                            ProyectoDetalleController proController = (ProyectoDetalleController) AppContext.getInstance().get("ControllerProyecto");
+                            proController.cargarTabla();
+                        }
+                        Mensaje.showAndWait(Alert.AlertType.INFORMATION, "Registro de tarea", "La tarea fue registrada con éxito");
+                        Stage stage = (Stage) btnGuardar.getScene().getWindow();
+                        stage.close();
+                    }else{
+                        Mensaje.showAndWait(Alert.AlertType.ERROR, "Registro de tarea", "Hubo un error al registrar la tarea");
+                    }
+                }
+            }
+            
+        }
+    }
+
+    @FXML
+    private void actNivelImportancia(ActionEvent event) {
+        establecerPrioridad();
+    }
+
+    @FXML
+    private void actNivelUrgencia(ActionEvent event) {
+        establecerPrioridad();
+    }
+    
+    public boolean validar(){
+        String titulo="";
+        if(modalidad.equals("Agregar")){
+            titulo = "Registro de tarea";
+        }else{
+            titulo = "Modificación de tarea";
+        }
+        
+        if(txtNombre.getText().isBlank()){
+            Mensaje.showAndWait(Alert.AlertType.WARNING, titulo, "Faltan datos por ingresar: nombre");
+            return false;
+        }
+        if(dpFechaInicio.getValue()==null){
+            Mensaje.showAndWait(Alert.AlertType.WARNING, titulo, "Faltan datos por ingresar: fecha de inicio");
+            return false;
+        }
+        if(dpFechaFinalizacion.getValue()==null){
+            Mensaje.showAndWait(Alert.AlertType.WARNING, titulo, "Faltan datos por ingresar: fecha de finalizacion");
+            return false;
+        }
+        if(cbNivelImportancia.getValue()==null){
+            Mensaje.showAndWait(Alert.AlertType.WARNING, titulo, "Faltan datos por ingresar: nivel de importancia");
+            return false;
+        }
+        if(cbNivelUrgencia.getValue()==null){
+            Mensaje.showAndWait(Alert.AlertType.WARNING, titulo, "Faltan datos por ingresar: nivel de urgencia");
+            return false;
+        }
+        return true;
+    }
+    
+    @JsonbDateFormat(value = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private Date fechaInicioGuardar;
+    
+    @JsonbDateFormat(value = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private Date fechaFinalizacionGuardar;
+
+    @FXML
+    private void actSelFechaInicio(ActionEvent event) {
+        Calendar dateCalendar = Calendar.getInstance();
+        String fecha[]=dpFechaInicio.getValue().toString().split("-");
+        dateCalendar.set(Calendar.YEAR,Integer.valueOf(fecha[0]));
+        dateCalendar.set(Calendar.MONTH, Integer.valueOf(fecha[1])-1);
+        dateCalendar.set(Calendar.DAY_OF_MONTH, Integer.valueOf(fecha[2]));
+        fechaInicioGuardar=dateCalendar.getTime();
+    }
+
+    @FXML
+    private void actSelFechaFinalizacion(ActionEvent event) {
+        Calendar dateCalendar = Calendar.getInstance();
+        String fecha[]=dpFechaFinalizacion.getValue().toString().split("-");
+        dateCalendar.set(Calendar.YEAR,Integer.valueOf(fecha[0]));
+        dateCalendar.set(Calendar.MONTH, Integer.valueOf(fecha[1])-1);
+        dateCalendar.set(Calendar.DAY_OF_MONTH, Integer.valueOf(fecha[2]));
+        fechaFinalizacionGuardar=dateCalendar.getTime();
+    }
     
 }
