16be42ab68ab07f74e1c826f22256a14569f3b7a
Jeffry99 <54681342+Jeffry99@users.noreply.github.com>
2020-10-31 00:08:26 -0600

    Update

--- cliente/src/main/java/org/una/examen/cliente/controller/ProvinciasTreeViewController.java
+++ cliente/src/main/java/org/una/examen/cliente/controller/ProvinciasTreeViewController.java
@@ -46,60 +46,6 @@ public class ProvinciasTreeViewController implements Initializable {
         // TODO
         
     }
-//        TreeItem root = new TreeItem<>("Root");
-//        treeView.setRoot(root);
-//        treeView.setShowRoot(false);
-//        ArrayList<ProvinciaDTO> provincias = new ArrayList<ProvinciaDTO>();
-//        Respuesta respuesta = provinciaService.getAll();
-//        if(respuesta.getEstado()){
-//            provincias = (ArrayList<ProvinciaDTO>)respuesta.getResultado("Provincias");
-//        }
-//        if(!provincias.isEmpty()){
-//            for(int i=0; i<provincias.size(); i++){
-//                ProvinciaDTO p = provincias.get(i);
-//                TreeItem item = new TreeItem("Provincia: "+p.getNombre());
-//                root.getChildren().add(item);
-//                
-//                ArrayList<CantonDTO> cantones = new ArrayList<CantonDTO>();
-//                Respuesta respuesta2 = cantonService.getByProvincia(p.getId());
-//                if(respuesta2.getEstado()){
-//                    cantones = (ArrayList<CantonDTO>)respuesta.getResultado("Cantones");
-//                }
-//                if(!cantones.isEmpty()){
-//                    for(int j=0; j<cantones.size(); j++){
-//                        CantonDTO c = cantones.get(j);
-//                        TreeItem item2 = new TreeItem("Cantón: "+c.getNombre());
-//                        item.getChildren().add(item);
-//                        
-////                        ArrayList<DistritoDTO> distritos = new ArrayList<DistritoDTO>();
-////                        Respuesta respuesta3 = distritoService.getByCanton(c.getId());
-////                        if(respuesta3.getEstado()){
-////                            distritos = (ArrayList<DistritoDTO>)respuesta.getResultado("Distritos");
-////                        }
-////                        if(!distritos.isEmpty()){
-////                            for(int k=0; k<distritos.size(); k++){
-////                                DistritoDTO d = distritos.get(k);
-////                                TreeItem item3 = new TreeItem("Distrito: "+d.getNombre());
-////                                item2.getChildren().add(item);
-//                                
-////                                ArrayList<UnidadDTO> unidades = new ArrayList<UnidadDTO>();
-////                                Respuesta respuesta4 = unidadService.getByDistrito(d.getId());
-////                                if(respuesta4.getEstado()){
-////                                    unidades = (ArrayList<UnidadDTO>)respuesta.getResultado("Unidades");
-////                                }
-////                                if(!provincias.isEmpty()){
-////                                    for(int l=0; l<provincias.size(); l++){
-////                                        UnidadDTO u = unidades.get(l);
-////                                        TreeItem item4 = new TreeItem("Unidad: "+u.getNombre());
-////                                        item3.getChildren().add(item);
-////                                    } 
-////                                }
-//                            } 
-//                        }
-//                    } 
-//                }
-//            } 
-//        }
-    //}
+}
 
 
